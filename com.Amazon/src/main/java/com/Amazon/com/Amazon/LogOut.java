815feadf004e39728b573331f6a9da902987eac9
dhanshree271089 <dhanshree.verma16@gmail.com>
2024-06-16 23:06:54 +0530

    LogOut Feature Added

--- com.Amazon/src/main/java/com/Amazon/com/Amazon/LogOut.java
+++ com.Amazon/src/main/java/com/Amazon/com/Amazon/LogOut.java
@@ -2,4 +2,8 @@ package com.Amazon.com.Amazon;
 
 public class LogOut {
 
+	public static void main(String[] args) {
+		System.out.println("Hello");
+	}
+
 }
--- com.Amazon/src/test/java/com/Amazon/com/Amazon/LogOutTest.java
+++ com.Amazon/src/test/java/com/Amazon/com/Amazon/LogOutTest.java
@@ -2,4 +2,7 @@ package com.Amazon.com.Amazon;
 
 public class LogOutTest {
 
+	public static void main(String[] args) {
+		System.out.println("Hello");
+	}
 }
